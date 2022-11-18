dedb775564de6a899ee84791f9160c85dceffa19
ARCHANA27101999 <kparchana2@gmail.com>
2022-11-18 11:21:01 +0530

    git kollaam

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Save from './Save';
+import Kollaam from './Kollaam';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Save />
+    <Kollaam />
   </React.StrictMode>
 );
 
